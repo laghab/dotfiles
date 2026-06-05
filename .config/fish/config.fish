53b1c89218a0e3c80d69667071ba9f5199cffe2f
laghab <laghab@users.noreply.github.com>
2026-06-05 12:03:21 +0530

    Initial dotfiles - full KDE config, themes, kitty, fish, zed, vlc, micro, systemd, gtk, mpv, opencode, yt-dlp, and more

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+alias btop "btop --force-utf"
+alias ytm "~/.local/bin/ytm"
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
